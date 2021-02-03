0329a1aa1ed6c40a3e91a2a0340b89222b1420b8
somer81 <sevinc.omer@gmail.com>
2021-02-03 18:15:44 +0300

    initial commit

---
+++ ConsoleUI/Program.cs
@@ -0,0 +1,21 @@
+﻿using Business.Concrete;
+using DataAccess.Abstract;
+using DataAccess.Concrete.InMemory;
+using System;
+
+namespace ConsoleUI
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CarManager carManager = new CarManager(new InMemoryCarDal());
+            var result = carManager.GetAll();
+
+            foreach (var car in result)
+            {
+                Console.WriteLine("{0} - {1} - {2} ", car.Id, car.Description, car.ModelYear);
+            }
+        }
+    }
+}
