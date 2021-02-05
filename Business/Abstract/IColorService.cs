58ba6f090296363ac8bc51f6472a8447f7bfad59
somer81 <sevinc.omer@gmail.com>
2021-02-05 22:27:28 +0300

    updates with brand and color manager and services

--- Business/Abstract/IColorService.cs
+++ Business/Abstract/IColorService.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Entities.Concrete;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +7,12 @@ namespace Business.Abstract
 {
     public interface IColorService
     {
+        Color GetById(int id);
+        List<Color> GetCarsByBrandId(int id);
+        List<Color> GetCarsByColorId(int id);
+        List<Color> GetAll();
+        void Add(Color color);
+        void Update(Color color);
+        void Delete(Color color);
     }
 }
