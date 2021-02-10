c1548a669ca1cb0b182ba64eaa901b8131211c5a
somer81 <sevinc.omer@gmail.com>
2021-02-10 18:14:50 +0300

    updates DTO

---
+++ Entities/DTOs/CarDetailsDto.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Entities.DTOs
+{
+    class CarDetailsDto
+    {
+        public int Id { get; set; }
+        public int BrandName { get; set; }
+        public int ColorName { get; set; }
+        public decimal DailyPrice { get; set; }
+    }
+}
